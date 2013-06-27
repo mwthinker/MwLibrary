d5646c352a3b736033a9a70fb96c56f80f4443f5
Marcus Welander <marcus85w@hotmail.com>
2013-06-27 19:22:39 +0200

    The vs2012 project changed.
    No solution folder. And only core sdl wrapper classes remainging. New
    help libraries created named MwExtra and MwEnet.

--- updateSdkFolder.bat
+++ updateSdkFolder.bat
@@ -1,33 +1,20 @@
 @echo off
-:: Copies the latest MwLibrary to SDK.
+:: Copies the latest MwLibrary to SDK folder.
 
-set solutionFolderVar="..\"
-set destFolder="C:\Programmering\SDK\mw"
+set destFolder="..\SDK\MwLibrary"
 
 del %destFolder%\include\mw\ /Q
-del %destFolder%\include\mw\signals\ /Q
-del %destFolder%\include\mw\enet\ /Q
-
 del %destFolder%\source\ /Q
-del %destFolder%\source\signals\ /Q
-del %destFolder%\source\enet\ /Q
 
 del %destFolder%\lib-r\ /Q
 del %destFolder%\lib-d\ /Q
 
-xcopy %solutionFolderVar%\MwLibrary\mw\*.h %destFolder%\include\mw\
-xcopy %solutionFolderVar%\MwLibrary\mw\*.cpp %destFolder%\source\
+xcopy mw\*.h %destFolder%\include\mw\
+xcopy mw\*.cpp %destFolder%\source\
 
-xcopy %solutionFolderVar%\MwLibrary\mw\signals\*.h %destFolder%\include\mw\signals\
-xcopy %solutionFolderVar%\MwLibrary\mw\signals\*.cpp %destFolder%\source\signals\
+xcopy Release\*.pdb %destFolder%\lib-r\
+xcopy Release\MwLibrary.lib %destFolder%\lib-r\
 
-xcopy %solutionFolderVar%\MwLibrary\mw\enet\*.h %destFolder%\include\mw\enet\
-xcopy %solutionFolderVar%\MwLibrary\mw\enet\*.cpp %destFolder%\source\enet\
-
-xcopy %solutionFolderVar%\MwLibrary\Release\*.pdb %destFolder%\lib-r\
-xcopy %solutionFolderVar%\Release\MwLibrary.lib %destFolder%\lib-r\
-xcopy %solutionFolderVar%\MwLibrary\Debug\*.pdb %destFolder%\lib-d\
-xcopy %solutionFolderVar%\MwLibrary\Debug\*.idb %destFolder%\lib-d\
-xcopy %solutionFolderVar%\Debug\MwLibrary.lib %destFolder%\lib-d\
-
-::"c:\Program Files\7-Zip\7z.exe" a "%destFolder%\mw.7z" "%destFolder%\"
+xcopy Debug\*.pdb %destFolder%\lib-d\
+xcopy Debug\*.idb %destFolder%\lib-d\
+xcopy Debug\MwLibrary.lib %destFolder%\lib-d\
